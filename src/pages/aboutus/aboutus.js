3022b185c88a28dcec56ee5712b1d96be033021c
Aura Salazar <aurasader23@gmail.com>
2026-05-29 15:36:00 -0600

    se guardaron cambios

--- src/pages/aboutus/aboutus.js
+++ src/pages/aboutus/aboutus.js
@@ -1,9 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 776aa07 (se guardaron cambios)
 // fetch("../../components/NavBar.html")
 //   .then(response => response.text())
 //   .then(data => {
 //     document.getElementById("navbar").innerHTML = data;
 //   });
+<<<<<<< HEAD
 
 // fetch("../../components/Footer.html")
 //   .then(response => response.text())
@@ -23,3 +27,11 @@ fetch("../../components/Footer.html")
     document.getElementById("footer").innerHTML = data;
   });
 >>>>>>> 01af8d5 (se guardaron cambios)
+=======
+
+// fetch("../../components/Footer.html")
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+>>>>>>> 776aa07 (se guardaron cambios)

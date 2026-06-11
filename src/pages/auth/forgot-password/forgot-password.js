53f1b401645540f742459542c5894e590025aba1
ivonne estrada <ivonne.estrada.glz@gmail.com>
2026-06-11 11:52:06 -0600

    logica login en archivo js

---
+++ src/pages/auth/forgot-password/forgot-password.js
@@ -0,0 +1,71 @@
+// manda a llamamar html
+
+document.addEventListener("DOMContentLoaded", () => {
+  const formularioLogin = document.getElementById("loginForm");
+  // valida ingreso de credenciales
+  if (formularioLogin) {
+    formularioLogin.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      //almacena informacion para validarla
+      const inputCorreo = document.getElementById("email");
+      const inputContrasena = document.getElementById("password");
+
+      const correo = inputCorreo.value.trim();
+      const contrasena = inputContrasena.value;
+
+      // valida y limpia para escribir d enuevo
+      let formularioCorrecto = true;
+
+      [inputCorreo, inputContrasena].forEach((input) => {
+        if (input) input.classList.remove("is-invalid", "is-valid");
+      });
+      // valida correo
+      const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!regexCorreo.test(correo)) {
+        if (inputCorreo) inputCorreo.classList.add("is-invalid");
+        formularioCorrecto = false;
+      } else {
+        if (inputCorreo) inputCorreo.classList.add("is-valid");
+      }
+      // calida contraseña
+      const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+      if (!regexPassword.test(contrasena)) {
+        if (inputContrasena) inputContrasena.classList.add("is-invalid");
+        formularioCorrecto = false;
+      } else {
+        if (inputContrasena) inputContrasena.classList.add("is-valid");
+      }
+      // envia si esta correcto
+      if (formularioCorrecto) {
+        const credencialesUsuario = {
+          email: correo,
+          password: contrasena,
+          recordarme: document.getElementById("rememberMe")?.checked || false, // Por si añades el checkbox de "Recordarme"
+        };
+
+        autenticarUsuario(credencialesUsuario);
+      } else {
+        alert("Por favor, verifica que los campos tengan el formato correcto.");
+      }
+    });
+
+    // envio a back
+    function autenticarUsuario(credenciales) {
+      console.log("Listo para autenticar en el backend:", credenciales);
+      // Aquí irá tu fetch() hacia la API de inicio de sesión en el futuro
+    }
+  }
+});
+
+// footer
+fetch("../../components/Footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    const footerContainer = document.getElementById("footer");
+    if (footerContainer) {
+      footerContainer.innerHTML = data;
+    }
+  })
+
+  .catch((err) => console.error("Error al cargar el footer:", err));

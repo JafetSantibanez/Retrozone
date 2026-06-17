b23fb364a29843a20f79f95f7716b5622296145a
Jafet <129018499+JafetSantibanez@users.noreply.github.com>
2026-06-17 10:30:23 -0600

    Revert "Dev ivonne"

--- src/pages/aboutus/aboutus.js
+++ src/pages/aboutus/aboutus.js
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 776aa07 (se guardaron cambios)
+// fetch("../../components/NavBar.html")
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById("navbar").innerHTML = data;
+//   });
+<<<<<<< HEAD
+
+// fetch("../../components/Footer.html")
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+=======
+fetch("../../components/NavBar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
+fetch("../../components/Footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
+>>>>>>> 01af8d5 (se guardaron cambios)
+=======
+
+// fetch("../../components/Footer.html")
+//   .then(response => response.text())
+//   .then(data => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+>>>>>>> 776aa07 (se guardaron cambios)
+=======
 fetch("../../components/NavBar.html")
   .then(response => response.text())
   .then(data => {
@@ -10,3 +49,4 @@ fetch("../../components/NavBar.html")
       document.getElementById("navbar").innerHTML = data;
     }
   });
+>>>>>>> origin/Andriy
